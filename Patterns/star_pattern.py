b8dc3bd57be92bb50c57fab5aa5b46bedb32696d
Varun Panchal <varunpanchal1008@gmail.com>
2026-01-29 21:23:41 +0530

    update

---
+++ Patterns/star_pattern.py
@@ -0,0 +1,8 @@
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(i,end=" ") 
+    print()
+    
+# outer loop will run for no of rows
+# inner loop will run how many stars u need in each row basically it
+# run for coumns
--- Random modules/mod1.py
+++ Random modules/mod1.py
@@ -3,7 +3,21 @@ import random
 #  randint()- return random int between a nd b 
 #  choice()- return a random item from the sequence
 
-fruits=['apple','banana','chiku']
-print(random.random()) 
-print(random.randint(20,25))
-print(random.choice(fruits))
+# fruits=['apple','banana','chiku']
+# print(random.random()) 
+# print(random.randint(20,25))
+# print(random.choice(fruits))
+
+## """"Create a program that roll a dice randomly"""""
+ 
+a="Click"
+print("Welcome to tha game :)")
+while True:
+    choice=input("Type 'Click' To roll the dice or 'q' tp quit:")
+    if choice == a:
+        number=random.randint(1,6)
+        print(f" The no u got is: {number}")
+    elif choice == 'q':
+        print("Thanks For Playing The game")
+    else:
+        print("INVALID INPUT")
